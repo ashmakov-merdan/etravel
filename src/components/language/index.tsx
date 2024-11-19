931ba2915371588e27a8d83a68fbe6cd421af1f0
Merdan Ashmakov <ashmakov.m@gmail.com>
2024-11-19 18:36:02 +0500

    fixed language icon

--- src/components/language/index.tsx
+++ src/components/language/index.tsx
@@ -1,4 +1,3 @@
-import TranslateIcon from "@/lib/icons/TranslateIcon";
 import { AnimatePresence, motion } from "framer-motion"
 import { usePathname, useRouter } from "@/i18n/navigation";
 import { startTransition, useState } from "react";
@@ -8,6 +7,7 @@ const Language = () => {
   const router = useRouter()
   const pathname = usePathname()
   const params = useParams()
+  const { locale } = params;
   const [show, setShow] = useState<boolean>(false);
 
   const onChangeLocale = (value: string) => {
@@ -25,13 +25,13 @@ const Language = () => {
   return (
     <div className="flex justify-end z-50">
       <button className="p-3 rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/30" onClick={() => setShow(!show)}>
-        <TranslateIcon color={"#222E9A"} />
+        <p>{locale.toString().toUpperCase()}</p>
       </button>
       <AnimatePresence>
         {show ? <motion.ul className="absolute space-y-2 text-primary" animate={{ y: 60, opacity: 1 }} exit={{ y: 0, opacity: 0 }}>
-          <motion.li onClick={() => onChangeLocale("en")} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">EN</motion.li>
-          <motion.li onClick={() => onChangeLocale("ru")} transition={{ duration: 0.2 }} initial={{y: -50}} animate={{ y: 0 }} exit={{ y: -50 }} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">RU</motion.li>
-          <motion.li onClick={() => onChangeLocale("tm")} transition={{ duration: 0.2 }} initial={{y: -100}} animate={{ y: 0 }} exit={{ y: -100 }} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">TM</motion.li>
+          {locale.toString().toLowerCase() !== "en" && <motion.li onClick={() => onChangeLocale("en")} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">EN</motion.li>}
+          {locale.toString().toLowerCase() !== "ru" && <motion.li onClick={() => onChangeLocale("ru")} transition={{ duration: 0.2 }} initial={{y: -50}} animate={{ y: 0 }} exit={{ y: -50 }} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">RU</motion.li>}
+          {locale.toString().toLowerCase() !== "tm" && <motion.li onClick={() => onChangeLocale("tm")} transition={{ duration: 0.2 }} initial={{y: -100}} animate={{ y: 0 }} exit={{ y: -100 }} className="w-12 h-12 grid place-items-center rounded-full border border-neutral-500 hover:border-primary backdrop-blur-md bg-white/40 cursor-pointer">TM</motion.li>}
         </motion.ul> : null}
       </AnimatePresence>
     </div>
